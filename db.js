86348e6197cb5023754acd1940d9e4e726b44580
Peterson Rodrigues <petersonpepi16@gmail.com>
2023-11-13 18:07:35 -0300

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,11 @@
 const Pool = require('pg').Pool
 
 const pool = new Pool({
-    user: 'postgres',
-    password: '12345678',
-    host: 'localhost',
+    user: 'user',
+    password: 'ZfupvLgk7KtW4gvnHp7p9I9Z60AEbFUz',
+    host: 'dpg-cl98k3v6e7vc73dar3a0-a',
     port: 5432,
-    database: "todolist"
+    database: "databse_1hpt"
 })
 
 module.exports = pool;
